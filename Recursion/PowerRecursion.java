a165c46ab50b1272244ba4f21a4a785af33083a3
Sodiq Sulaimon <sodiq.sulaimon204@gmail.com>
2024-05-10 20:51:23 -0500

    Updated the implemention of power of m raised to n with recursion

--- Recursion/PowerRecursion.java
+++ Recursion/PowerRecursion.java
@@ -6,7 +6,18 @@ public class PowerRecursion {
         return power(m, n - 1) * m;
     }
 
+    public static int power2(int m, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n % 2 == 0 ) {
+            return power2(m * m, n/2);
+        }
+        return m * power2(m * m, (n - 1)/2);
+    }
+
     public static void main(String[] args) {
-        System.out.println(power(2, 9));
+        System.out.println(power(3, 4));
+        System.out.println(power2(3, 4));
     }
 }
