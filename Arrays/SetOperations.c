096d161044fda74099f7ed81a6724d7c7b9544c7
Sodiq Sulaimon <sodiq.sulaimon204@gmail.com>
2024-05-22 09:33:26 -0500

    Added a function that returns the union of two sorted arrays

--- Arrays/SetOperations.c
+++ Arrays/SetOperations.c
@@ -9,7 +9,7 @@ struct Array
 };
 
 // Function to check if an element is present in the array
-int isPresent(struct Array *arr, int element) {
+int IsPresent(struct Array *arr, int element) {
     for (int i = 0; i < arr->length; i++) {
         if (arr->A[i] == element) {
             return 1;
@@ -27,19 +27,56 @@ struct Array* Union1(struct Array *arr1, struct Array *arr2) {
     arr3->length = 0;
     arr3->size = arr1->size + arr2->size;
 
-    for (i=0; i<arr1->length; i++) {
+    for (i=0; i < arr1->length; i++) {
         arr3->A[k++] = arr1->A[i];
     }
     arr3->length = k;
 
     for (j=0; j<arr2->length; j++) {
-        if (!(isPresent(arr1, arr2->A[j]))) {
+        if (!(IsPresent(arr1, arr2->A[j]))) {
             arr3->A[k++] = arr2->A[j];
         }
 
     }
     arr3->length = k;
 
+    return arr3;
+    free(arr3);
+    arr3 = NULL;
+}
+
+// union for sorted arrays
+struct Array* Union2(struct Array *arr1, struct Array *arr2) {
+    int i=0;
+    int j=0;
+    int k=0;
+
+    struct Array *arr3 = (struct Array *) malloc(sizeof(struct Array));
+    arr3->size = arr1->size + arr2->size;
+    arr3->length = 0;
+
+    while (i < arr1->length && j < arr2->length) {
+        if (arr1->A[i] < arr2->A[j]) {
+            arr3->A[k++] = arr1->A[i++];
+        }
+        else if (arr1->A[i] > arr2->A[j]) {
+            arr3->A[k++] = arr2->A[j++];
+        }
+        else {
+            arr3->A[k++] = arr1->A[i++];
+            j++;
+        }
+    }
+    // copy the remaining elements if any
+    for (; i < arr1->length; i++) {
+        arr3->A[k++] = arr1->A[i];
+    }
+
+    for (; j < arr2->length; j++) {
+        arr3->A[k++] = arr2->A[j];
+    }
+
+    arr3->length = k;
     return arr3;
     free(arr3);
     arr3 = NULL;
@@ -56,11 +93,16 @@ void Display (struct Array arr)
 
 int main()
 {
-    struct Array arr1 = {{3, 2, 4, 6, 8, 10}, 10, 6};
-    struct Array arr2 = {{6, 4, 9, 8, 1}, 10, 5};
+//    struct Array arr1 = {{3, 2, 4, 6, 8, 10}, 10, 6};
+//    struct Array arr2 = {{6, 4, 9, 8, 1}, 10, 5};
+//    struct Array *arr3 = Union1(&arr1, &arr2);
 
-   struct Array *arr3 = Union1(&arr1, &arr2);
-   Display(*arr3);
+    struct Array arr3 = {{2, 3, 4, 6, 8, 10}, 10, 6};
+    struct Array arr4 = {{1, 4, 6, 8, 9, 12}, 10, 6};
+    struct Array *arr5 = Union2(&arr3, &arr4);
+
+//    Display(*arr3);
+    Display(*arr5);
 
     return 0;
 }
