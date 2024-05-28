1dab383989c59ac2b44f98843651f8b9c897c62f
Sodiq Sulaimon <sodiq.sulaimon204@gmail.com>
2024-05-28 13:42:22 -0500

    Created a linkedlist and displayed it

---
+++ LinkedList/CreateLinkedList.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next; // Node is self-referential
+}*first = NULL;
+
+void Create(int elements[], int n) {
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = elements[0];
+    first->next = NULL;
+
+    struct Node *current, *last;
+    last = first;
+
+    for(int i=1; i < n; i++) {
+        current = (struct Node *)malloc(sizeof(struct Node));
+        current->data = elements[i];
+        current->next = NULL;
+        last->next = current;
+        last = current;
+    }
+};
+
+void Display(struct Node *p) {
+    while (p != NULL) {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+
+}
+
+// Display recursively
+void RDisplay(struct Node * p) {
+    if (p != NULL) {
+        printf("%d ", p->data);
+        RDisplay(p->next);
+    }
+
+}
+
+int main()
+{
+    int arr[] = {3, 2, 9, 4, 6};
+    Create(arr, 5);
+    Display(first);
+    printf("\n");
+    RDisplay(first);
+
+    return 0;
+}
+
